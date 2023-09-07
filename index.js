61e3a7c53a2ed1d9eafb411e3441df09ee3214e9
roobottom <roo@roodesign.co.uk>
2023-09-07 18:13:31 +0100

    Now takes data from a webform and writes md to github

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const basicAuth = require('./lib/auth.js'); 
-
+const createDiaryPostRoute = require('./lib/routes/diary-create.js');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(basicAuth);
+app.use(express.static('app'));
 
 nunjucks.configure('app', { //the name of the folder with the njk files
   autoescape: true,
@@ -16,6 +18,8 @@ app.set('view engine', 'njk');
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.use('/diary-create', createDiaryPostRoute);
 
 // app.get('/diary', (req, res) => {
 //   res.send('Diary posts overview');
---
+++ posts/This is cheese.md
@@ -0,0 +1,12 @@
+---
+tags:
+  - gy
+  - djshd
+  - dsjh
+---
+hello, this is _markdown_
+
+dskasad
+asd
+a
+sd
