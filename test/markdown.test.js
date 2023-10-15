38abd9d4f56b618267c059e3c2144ac264846eb6
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-10-15 21:19:50 +0100

    Use built-in Node test runner

--- test/markdown.test.js
+++ test/markdown.test.js
@@ -1,55 +1,59 @@
-const { createMarkdown } = require('../lib/markdown'); // Change the path accordingly
+const assert = require('node:assert');
+const test = require('node:test');
+const { createMarkdown } = require('../lib/markdown');
 const matter = require('gray-matter');
 
 test('Test createMarkdown with minimal options', () => {
   const data = {
-    content: "Hello, this is content."
+    content: 'Hello, this is content.'
   };
+  const markdown = createMarkdown(data);
+  const result = matter(markdown);
 
-  const result = createMarkdown(data);
-  const parsedResult = matter(result);
-
-  expect(parsedResult.content.trim()).toBe("Hello, this is content.");
+  assert.equal(result.content.trim(), "Hello, this is content.");
 });
 
 test('Test createMarkdown with some frontmatter', () => {
   const data = {
-    content: "More content here",
+    content: 'More content here',
     summary: 'This is a summary',
     date: '2023-09-26'
   };
+  const markdown = createMarkdown(data);
+  const result = matter(markdown);
 
-  const result = createMarkdown(data);
-  const parsedResult = matter(result);
-
-  expect(parsedResult.data.summary).toBe('This is a summary');
-  expect(parsedResult.data.date).toEqual(new Date('2023-09-26'));
+  assert.equal(result.data.summary, 'This is a summary');
+  assert.equal(result.data.date, new Date('2023-09-26').toString());
 });
 
 test('Test createMarkdown with photos and tags', () => {
   const data = {
-    content: "Even more content",
-    photos: [{ remoteUrl: '/assets/images/diary/photo1.jpg', alt: 'photo1' }],
+    content: 'Even more content',
+    photos: [{
+      remoteUrl: '/assets/images/diary/photo1.jpg',
+      alt: 'photo1'
+    }],
     tags: 'tag1, tag2'
   };
+  const markdown = createMarkdown(data);
+  const result = matter(markdown);
 
-  const result = createMarkdown(data);
-  const parsedResult = matter(result);
-
-  expect(parsedResult.data.photo[0]).toEqual({ url: '/assets/images/diary/photo1.jpg', alt: 'photo1' });
-  expect(parsedResult.data.tags).toEqual(['tag1', 'tag2']);
+  assert.deepEqual(result.data.photo[0], {
+    url: '/assets/images/diary/photo1.jpg',
+    alt: 'photo1'
+  });
+  assert.deepEqual(result.data.tags, ['tag1', 'tag2']);
 });
 
 test('Test createMarkdown with a boolean and an object', () => {
   const data = {
-    content: "Let's add some booleans and objects",
+    content: 'Let’s add some booleans and objects',
     isDraft: true,
     metadata: { author: 'Jon' }
   };
+  const markdown = createMarkdown(data);
+  const result = matter(markdown);
 
-  const result = createMarkdown(data);
-  const parsedResult = matter(result);
-
-  expect(parsedResult.data.isDraft).toBe(true);
-  expect(parsedResult.data.metadata).toBe(JSON.stringify({ author: 'Jon' }));
+  assert.equal(result.data.isDraft, true);
+  assert.equal(result.data.metadata, JSON.stringify({ author: 'Jon' }));
 });
